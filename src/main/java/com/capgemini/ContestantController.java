7eaead67adecb083a6450e8dcc8a16747622607a
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-04-20 12:06:59 +0200

    Added 24 players with stats

--- src/main/java/com/capgemini/ContestantController.java
+++ src/main/java/com/capgemini/ContestantController.java
@@ -2,30 +2,30 @@ package com.capgemini;
 
 public class ContestantController {
     //Initiated all the playes
-    Contestant playerDistrict1 = new District(true,100,Gender.male, 100, 100,5);
-    Contestant playerDistrict2 = new District(true,100,Gender.male, 32, 18,12);
-    Contestant playerDistract3 = new District(true,100,Gender.male,43,72,4);
-    Contestant playerDistrict4 = new District(true,100,Gender.male, 22, 11,87);
-    Contestant playerDistract5 = new District(true,100,Gender.male,55,78,4);
-    Contestant playerDistrict6 = new District(true,100,Gender.male, 53, 32,14);
-    Contestant playerDistract7 = new District(true,100,Gender.male,88,28,45);
-    Contestant playerDistrict8 = new District(true,100,Gender.male, 3, 25, 8);
-    Contestant playerDistract9 = new District(true,100,Gender.male,66,21,9);
-    Contestant playerDistract10 = new District(true,100,Gender.female,24,21,9);
-    Contestant playerDistract11 = new District(true,100,Gender.female,66,55,2);
-    Contestant playerDistract12 = new District(true,100,Gender.female,88,2,47);
-    Contestant playerDistract13 = new District(true,100,Gender.female,67,25,65);
-    Contestant playerDistract14 = new District(true,100,Gender.female,73,75,87);
-    Contestant playerDistract15 = new District(true,100,Gender.female,22,74,55);
-    Contestant playerDistract16 = new District(true,100,Gender.female,6,69,23);
-    Contestant playerDistract17 = new District(true,100,Gender.female,47,11,88);
-    Contestant playerDistract18 = new District(true,100,Gender.female,56,8,4);
-    Contestant playerCareer19 = new Career(true,100,Gender.male, 55,26,24);
-    Contestant playerCareer20 = new Career(true,100,Gender.male, 2,5,59);
-    Contestant playerCareer21 = new Career(true,100,Gender.male, 54,47,57);
-    Contestant playerCareer22 = new Career(true,100,Gender.female, 53,35,13);
-    Contestant playerCareer23 = new Career(true,100,Gender.female, 24,22,5);
-    Contestant playerCareer24 = new Career(true,100,Gender.female, 98,58,78);
+    Contestant playerDistrict1 = new District(1, true,100,Gender.male, 100, 18, 2);
+    Contestant playerDistrict2 = new District(2, true,100,Gender.male, 32, 18,12);
+    Contestant playerDistract3 = new District(3,true,100,Gender.male,43,72,4);
+    Contestant playerDistrict4 = new District(4,true,100,Gender.male, 22, 11,87);
+    Contestant playerDistract5 = new District(5,true, 100,Gender.male,55,78,4);
+    Contestant playerDistrict6 = new District(6,true,100,Gender.male, 53, 32,14);
+    Contestant playerDistract7 = new District(7,true,100,Gender.male,88,28,45);
+    Contestant playerDistrict8 = new District(8,true,100,Gender.male, 3, 25, 8);
+    Contestant playerDistract9 = new District(9,true,100,Gender.male,66,21,9);
+    Contestant playerDistract10 = new District(10,true,100,Gender.female,24,21,9);
+    Contestant playerDistract11 = new District(11,true,100,Gender.female,66,55,2);
+    Contestant playerDistract12 = new District(12,true,100,Gender.female,88,2,47);
+    Contestant playerDistract13 = new District(13,true,100,Gender.female,67,25,65);
+    Contestant playerDistract14 = new District(14,true,100,Gender.female,73,75,87);
+    Contestant playerDistract15 = new District(15,true,100,Gender.female,22,74,55);
+    Contestant playerDistract16 = new District(16,true,100,Gender.female,6,69,23);
+    Contestant playerDistract17 = new District(17,true,100,Gender.female,47,11,88);
+    Contestant playerDistract18 = new District(18,true,100,Gender.female,56,8,4);
+    Contestant playerCareer19 = new Career(19,true,100,Gender.male, 55,26,24);
+    Contestant playerCareer20 = new Career(20,true,100,Gender.male, 2,5,59);
+    Contestant playerCareer21 = new Career(21,true,100,Gender.male, 54,47,57);
+    Contestant playerCareer22 = new Career(22,true,100,Gender.female, 53,35,13);
+    Contestant playerCareer23 = new Career(23,true,100,Gender.female, 24,22,5);
+    Contestant playerCareer24 = new Career(24,true,100,Gender.female, 98,58,78);
 
 
 
